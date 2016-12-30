9c5c321a3b6867579a59ce2aee9f876bd95824c3
pearpages <pere@soms.cat>
2016-12-30 17:03:00 +0100

    model driven form

--- src/app/form2/form2.component.ts
+++ src/app/form2/form2.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Form2Component implements OnInit {
 
+  // A FormControl takes 3 arguments. a default value, a validator and an asynchronous validator. All of them are optional.
+  contactForm = new FormGroup({
+    firstname: new FormControl(),
+    lastname: new FormControl(),
+    address: new FormGroup({
+      city: new FormControl(),
+      street: new FormControl(),
+      pincode: new FormControl()
+    })
+  })
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value);
   }
 
 }
