28c0855200e1b7a2d7554e7d62ada10f486df64b
pearpages <pere@soms.cat>
2016-12-30 13:02:10 +0100

    form example1

---
+++ src/app/form1/form1.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { Form1Component } from './form1.component';
+
+describe('Form1Component', () => {
+  let component: Form1Component;
+  let fixture: ComponentFixture<Form1Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Form1Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Form1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
